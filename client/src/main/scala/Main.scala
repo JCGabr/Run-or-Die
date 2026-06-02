5266aa94322cf2cea9808161c3551236cde42de0
JCGabr <cornejocastrojosegabriel@gmail.com>
2026-06-02 10:50:06 -0500

    feat(canvas): add canvas and select it on scala

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -1,10 +1,10 @@
 import org.scalajs.dom
+import org.scalajs.dom.html
 import org.scalajs.dom.document
 
 object Main:
     def main(args: Array[String]): Unit =
         document.addEventListener("DOMContentLoaded", {_ => 
-            val p = document.createElement("p")
-            p.textContent = "Hello World!"
-            document.body.appendChild(p)
+            val canvas = document.getElementById("window-game").asInstanceOf[html.Canvas]
+            val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
         })
