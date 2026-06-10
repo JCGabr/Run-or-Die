25bad558115df290614ad061cc5d973a0a6bc9ce
danielaperalesestrada <peralesdaniela82@gmail.com>
2026-06-09 19:01:16 -0500

    feat(drawer): add pattern-based segment rendering

--- client/src/main/scala/Drawer.scala
+++ client/src/main/scala/Drawer.scala
@@ -14,18 +14,55 @@ object Drawer:
         canvas.height = canvas.clientHeight
 
         // Calcular tileSize para que llene exactamente el canvas
-        val tileSizeX = canvas.width / 20
+        /*val tileSizeX = canvas.width / 20
         val tileSizeY = canvas.height / (10 + 1)
         val tileSize = math.min(tileSizeX, tileSizeY)
+        */
+        val totalCols = matriz.head.length * 2
+        val totalRows = matriz.length * 2
 
+        val tileSizeX = canvas.width / totalCols
+        val tileSizeY = canvas.height / totalRows
+
+        val tileSize = math.min(tileSizeX, tileSizeY)
+        
         matriz.zipWithIndex.foreach { case (row, y) =>
             row.zipWithIndex.foreach { case (segment, x) =>
-                ctx.fillStyle = segment.id match {
+
+                segment.pattern.zipWithIndex.foreach {
+                    case (patternRow, py) =>
+
+                        patternRow.zipWithIndex.foreach {
+                            case (cell, px) =>
+
+                                ctx.fillStyle = cell match {
+                                    case "G" => "#8B4513" // ground
+                                    case "P" => "#228B22" // platform
+                                    case "S" => "#FF0000" // spike
+                                    case "C" => "#FFFF00" // checkpoint
+                                    case _   => "#87CEEB" // vacío
+                                }
+
+                                ctx.fillRect(
+                                    (x * 2 + px) * tileSize,
+                                    (y * 2 + py) * tileSize,
+                                    tileSize,
+                                    tileSize
+                                )
+                        }
+                }
+            }
+        }
+
+        /*
+        matriz.zipWithIndex.foreach { case (row, y) =>
+            row.zipWithIndex.foreach { case (segment, x) =>
+                ctx.fillStyle = segment.familyId match {
                     case "ground"   => "#8B4513"
                     case "platform" => "#228B22"
                     case _          => "#87CEEB"
                 }
-                ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize)
+                //ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize)
             }
-        }
+        }*/
     }
