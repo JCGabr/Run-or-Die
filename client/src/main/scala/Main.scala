dc96eb69c841eed2af21ed1d635bc46a032ea20a
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2026-06-12 18:04:31 -0500

    feat(main): add player init and input handler setup

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -8,13 +8,25 @@ object Main:
         
         Drawer.render(delta_time)
         
-        Logic.update(delta_time)
+        Logic.update(InputHandler.getInput(1), delta_time)
 
         //llamada cuando el frame se encuentre disponible
         dom.window.requestAnimationFrame( dt => { gameLoop(GameLoopInfo(dt, loop_info.current_time)) })
     }
 
     def main(args: Array[String]): Unit = {
+        val initialPlayer = Player(
+            coords = Vector2(50f, 100f),
+            velocity = Vector2(0f, 0f),
+            current_time = 300f,
+            max_time = 30f,
+            stats = Constants.CHARACTERS("Gnome"),
+            is_grounded = false,
+            is_alive = true
+        )
+        Logic.init(initialPlayer, Drawer.cells)
+        
+        InputHandler.init()
         dom.window.requestAnimationFrame(startTime =>
             dom.window.requestAnimationFrame(dt =>
                 gameLoop(GameLoopInfo(dt, startTime))
