06bdb215729d1b0fe6a0de45574dc0ad9bf033e8
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2026-06-12 18:00:33 -0500

    feat(logic): add player update and init

--- shared/src/main/scala/Logic.scala
+++ shared/src/main/scala/Logic.scala
@@ -1,5 +1,14 @@
 
 object Logic:
-    def update(delta_time: Double): Unit = {
 
+    var player: Player = _
+    var map: Vector[Vector[String]] = _
+
+    def init(p: Player, m: Vector[Vector[String]]): Unit = {
+        player = p
+        map = m
+    }
+
+    def update(input: InputState, delta_time: Double): Unit = {
+        player = player.update(input, delta_time.toFloat, map)
     }
